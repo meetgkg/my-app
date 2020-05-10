a4da4cbc0ed9f2216f3fd224f0f6daa833c2c00b
meetgkg <gauravg.che@gmail.com>
2020-05-11 01:32:17 +0530

    code

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('Rohu Fish', 'Rohu Fish Recipe', 'https://upload.wikimedia.org/wikipedia/commons/d/d1/Labeo_rohita.JPG'),
+    new Recipe('Salmon Fish', 'Salmon Fish Recipe', 'https://cdn.pixabay.com/photo/2019/04/21/09/14/salmon-4143734_960_720.jpg')
+  ];
 
   constructor() { }
 
